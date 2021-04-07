7aca1b28aef6def5fcaf2394f61f3d439f70b814
Sagramielgo <sagramielgo@gmail.com>
2021-04-07 11:14:51 +0200

    add background image

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,12 @@ import '../stylesSheet/App.scss';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-container">
-          <h1>Las ovejas</h1>
-          <h2>Número total de ovejas: 0</h2>
-          <button className="form__btn" onClick={this.handleButton}>
-            Añadir oveja
-          </button>
-        </div>
+      <div className="App-container">
+        <h1>Sheeps counter</h1>
+        <h2>Number of sheeps: 0</h2>
+        <button className="form__btn" onClick={this.handleButton}>
+          One more sheep
+        </button>
       </div>
     );
   }
