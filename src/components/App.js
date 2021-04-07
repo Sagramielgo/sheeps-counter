6df4695af90454c63a0f5e111c49b5f5af8dccfd
Sagramielgo <sagramielgo@gmail.com>
2021-04-07 10:16:33 +0200

    add basic structure at App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,17 @@ import '../stylesSheet/App.scss';
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <div className="App-container">
+          <h1>Las ovejas</h1>
+          <h2>Número total de ovejas: 0</h2>
+          <button className="form__btn" onClick={this.handleButton}>
+            Añadir oveja
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
