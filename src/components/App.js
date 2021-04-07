a7b826b6f7691172c2e468f4d9016164f4a1cb41
Sagramielgo <sagramielgo@gmail.com>
2021-04-07 10:03:15 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../stylesSheet/App.scss';
+
+class App extends React.Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+
+export default App;
